902c2c441637875f6bcf26255e93369045f4fe1b
Matt Conzen <mattconzen@gmail.com>
2015-10-06 01:44:21 -0500

    Started basic version of main UI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the bookPlanApp
  */
 angular.module('bookPlanApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.books = [
+              { name: 'Hopscotch',
+                author: 'Julio Cortazar',
+                cover: 'images\\hopscotch.jpg',
+                length: 576,
+                year: 1966 },
+              { name: 'Gravity\'s Rainbow',
+                author: 'Thomas Pynchon',
+                cover: 'images\\gravitysrainbow.jpg',
+                length: 776,
+                year: 1973 }
     ];
+    $scope.getTotalPages = function($scope) {
+      var totalLength = 0;
+      for (var i = 0; i < $scope.books.length; i++) {
+        totalLength += $scope.books[i].length;
+      }
+      return totalLength;
+    };
   });
