058d6cbaa61c5778fa07ed46a21f0bb2e3f98140
Matt Conzen <mattconzen@gmail.com>
2015-10-06 00:10:45 -0500

    Initial commit after 'yo angular bookplan' and resolving Karma/Jasmine/PhantomJS dependencies.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookPlanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bookPlanApp
+ */
+angular.module('bookPlanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookPlanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bookPlanApp
+ */
+angular.module('bookPlanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
