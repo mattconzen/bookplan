b8a2523686600e340e4d2409f76fa3e026d49882
Matt Conzen <mattconzen@gmail.com>
2015-10-06 21:05:42 -0500

    Added support for multiple shelves...and completely broke sorting.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,21 +15,61 @@ angular.module('bookPlanApp')
                 author: 'Julio Cortazar',
                 cover: 'images\\hopscotch.jpg',
                 pages: 576,
-                year: 1966 },
+                year: 1966,
+                shelf: 'reading'},
               { title: 'Gravity\'s Rainbow',
                 author: 'Thomas Pynchon',
                 cover: 'images\\gravitysrainbow.jpg',
                 pages: 776,
-                year: 1973 }
+                year: 1973,
+                shelf: 'reading'},
+              { title: 'Naked Lunch',
+                author: 'William S. Burroughs',
+                cover: 'images\\nakedlunch.jpg',
+                pages: 411,
+                year: 1966,
+                shelf: 'to read'},
+              { title: 'A Naked Singularity',
+                author: 'Sergio De La Pava',
+                cover: 'images\\nakedsingularity.jpg',
+                pages: 652,
+                year: 2011,
+                shelf: 'to read' },
+              { title: 'M Train',
+                author: 'Patti Smith',
+                cover: 'images\\mtrain.jpg',
+                pages: 205,
+                year: 2015,
+                shelf: 'to read' },
+              { title: 'Wolf in White Van',
+                author: 'John Darnielle',
+                cover: 'images\\wolfinwhitevan.jpg',
+                pages: 652,
+                year: 2011,
+                shelf: 'to read' },
     ];
-    $scope.getTotalPages = function() {
+
+    $scope.getBooksByShelf = function(shelf){
+      var bookshelf=[];
+      var books = $scope.books;
+      for (var i = 0; i < books.length; i++){
+        if (books[i].shelf === shelf) {
+          bookshelf.push(books[i]);
+        }
+      }
+      return bookshelf;
+    };
+
+    $scope.getTotalPagesByShelf = function(shelf) {
       var totalLength = 0;
-      for (var i = 0; i < $scope.books.length; i++) {
-        totalLength += $scope.books[i].pages;
+      var books = $scope.getBooksByShelf(shelf);
+      for (var i = 0; i < books.length; i++) {
+        totalLength += books[i].pages;
       }
       return totalLength;
     };
-    $scope.getDaysLeft = function(){
-      return Math.round($scope.getTotalPages() / $scope.pagesPerDay);
+
+    $scope.getDaysLeftByShelf = function(shelf){
+      return Math.round($scope.getTotalPagesByShelf(shelf) / $scope.pagesPerDay);
     };
   });
